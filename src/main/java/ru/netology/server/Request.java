24cb634cb75d7c3190af08a399c5de72593c9091
Eugene Borisov <eugeni.borisov@gmail.com>
2023-02-12 20:07:45 +0300

    add query

--- src/main/java/ru/netology/server/Request.java
+++ src/main/java/ru/netology/server/Request.java
@@ -1,15 +1,25 @@
 package ru.netology.server;
 
+import org.apache.http.NameValuePair;
+
+import java.util.List;
+
 public class Request {
 
     private String method;
-    private String URI;
-    private String headers;
+    private String path;
+    private List<NameValuePair> queryParams;
+    private List<String> headers;
     private String body;
+    private String URI;
 
     public Request(String method, String URI) {
         this.method = method;
         this.URI = URI;
+    }
+
+    public Request() {
+
     }
 
     public String getMethod() {
@@ -20,11 +30,49 @@ public class Request {
         return URI;
     }
 
-    public String getHeaders() {
+    public List<String> getHeaders() {
         return headers;
     }
 
     public String getBody() {
         return body;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public List<NameValuePair> queryParams() {
+        return queryParams;
+    }
+
+    public String getQueryParam(String name) {
+        for (NameValuePair queryParam : queryParams()) {
+            if (name.equals(queryParam.getName())) {
+                return queryParam.getValue();
+            }
+        }
+        System.out.println("There is no such query parameter");
+        return null;
+    }
+
+    public void setQueryParams(List<NameValuePair> queryParams) {
+        this.queryParams = queryParams;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setHeaders(List<String> headers) {
+        this.headers = headers;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 }
